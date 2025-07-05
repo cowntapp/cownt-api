112f5ff5e65fccc4d75ada9fc118f5605d6e8f14
munergenis <munergenis@gmail.com>
2025-07-05 12:39:09 +0200

    chore: add comments to clarify functionality

--- src/features/animals/cow/service/breedMethods/updateBreed.ts
+++ src/features/animals/cow/service/breedMethods/updateBreed.ts
@@ -6,6 +6,7 @@ export async function updateBreed(
   breedId: string,
   breedData: UpdateBreedSchema
 ) {
+  // No need for transaction as animal only contains breedId
   const updatedBreed: CowBreed | null = await CowBreedModel.findByIdAndUpdate(
     breedId,
     breedData,
