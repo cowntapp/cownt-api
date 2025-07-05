2fe826866acad2afb5a80d7a9532825f7bc959aa
munergenis <munergenis@gmail.com>
2025-07-05 13:11:35 +0200

    feat: add breed existence check in updateBreed function for cow and sheep

--- src/features/animals/cow/service/breedMethods/updateBreed.ts
+++ src/features/animals/cow/service/breedMethods/updateBreed.ts
@@ -1,3 +1,5 @@
+import { CONFLICT } from '../../../../../lib/constants/http';
+import appAssert from '../../../../../lib/utils/appAssert';
 import { CowBreed, CowBreedModel } from '../../model/cow.model';
 
 import { UpdateBreedSchema } from '../../validation/cow.schemas';
@@ -6,6 +8,13 @@ export async function updateBreed(
   breedId: string,
   breedData: UpdateBreedSchema
 ) {
+  const exitingBreed = await CowBreedModel.find({ value: breedData.value });
+  appAssert(
+    exitingBreed.length === 0,
+    CONFLICT,
+    `Breed ${breedData.value} already exists`
+  );
+
   // No need for transaction as animal only contains breedId
   const updatedBreed: CowBreed | null = await CowBreedModel.findByIdAndUpdate(
     breedId,
--- src/features/animals/sheep/service/breedMethods/updateBreed.ts
+++ src/features/animals/sheep/service/breedMethods/updateBreed.ts
@@ -1,3 +1,5 @@
+import { CONFLICT } from '../../../../../lib/constants/http';
+import appAssert from '../../../../../lib/utils/appAssert';
 import { SheepBreed, SheepBreedModel } from '../../model/sheep.model';
 
 import { UpdateBreedSchema } from '../../validation/sheep.schemas';
@@ -6,6 +8,13 @@ export async function updateBreed(
   breedId: string,
   breedData: UpdateBreedSchema
 ) {
+  const existingBreed = await SheepBreedModel.find({ value: breedData.value });
+  appAssert(
+    existingBreed.length === 0,
+    CONFLICT,
+    `Breed ${breedData.value} already exists`
+  );
+
   // No need for transaction as animal only contains breedId
   const updatedBreed: SheepBreed | null =
     await SheepBreedModel.findByIdAndUpdate(breedId, breedData, {
