7d49226a79c81e99289c650f24d465f3463a35b0
munergenis <munergenis@gmail.com>
2025-07-05 08:54:57 +0200

    feat: add breed existence check in createBreed function for cow and sheep

--- src/features/animals/cow/service/breedMethods/createBreed.ts
+++ src/features/animals/cow/service/breedMethods/createBreed.ts
@@ -1,7 +1,13 @@
+import { CONFLICT } from '../../../../../lib/constants/http';
+import appAssert from '../../../../../lib/utils/appAssert';
 import { CowBreed, CowBreedModel } from '../../model/cow.model';
 import { CreateBreedSchema } from '../../validation/cow.schemas';
+import { checkBreedExistsByValue } from '../utils/validations';
 
 export async function createBreed(breedData: CreateBreedSchema) {
+  const breedExsits = await checkBreedExistsByValue(breedData.value);
+  appAssert(!breedExsits, CONFLICT, 'Breed already exists');
+
   const newBreed: CowBreed = await CowBreedModel.create(breedData);
   return newBreed;
 }
--- src/features/animals/sheep/service/breedMethods/createBreed.ts
+++ src/features/animals/sheep/service/breedMethods/createBreed.ts
@@ -1,7 +1,13 @@
+import { CONFLICT } from '../../../../../lib/constants/http';
+import appAssert from '../../../../../lib/utils/appAssert';
 import { SheepBreed, SheepBreedModel } from '../../model/sheep.model';
 import { CreateBreedSchema } from '../../validation/sheep.schemas';
+import { checkBreedExistsByValue } from '../utils/validations';
 
 export async function createBreed(breedData: CreateBreedSchema) {
+  const breedExists = await checkBreedExistsByValue(breedData.value);
+  appAssert(!breedExists, CONFLICT, 'Breed already exists');
+
   const newBreed: SheepBreed = await SheepBreedModel.create(breedData);
   return newBreed;
 }
