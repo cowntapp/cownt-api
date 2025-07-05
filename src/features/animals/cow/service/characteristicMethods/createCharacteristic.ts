5399660632ca34268432d2950c74d21af1cddb66
munergenis <munergenis@gmail.com>
2025-07-05 13:50:17 +0200

    feat: add characteristic existence check in createCharacteristic function for cow and sheep

--- src/features/animals/cow/service/characteristicMethods/createCharacteristic.ts
+++ src/features/animals/cow/service/characteristicMethods/createCharacteristic.ts
@@ -1,13 +1,21 @@
+import { CONFLICT } from '../../../../../lib/constants/http';
+import appAssert from '../../../../../lib/utils/appAssert';
 import {
   CowCharacteristic,
   CowCharacteristicModel,
 } from '../../model/cow.model';
 
 import { CreateCharacteristicSchema } from '../../validation/cow.schemas';
+import { checkCharacteristicExistsByValue } from '../utils/validations';
 
 export async function createCharacteristic(
   characteristicData: CreateCharacteristicSchema
 ) {
+  const characteristicExists = await checkCharacteristicExistsByValue(
+    characteristicData.value
+  );
+  appAssert(!characteristicExists, CONFLICT, 'Characteristic already exists');
+
   const newCharacteristic: CowCharacteristic =
     await CowCharacteristicModel.create(characteristicData);
   return newCharacteristic;
--- src/features/animals/sheep/service/characteristicMethods/createCharacteristic.ts
+++ src/features/animals/sheep/service/characteristicMethods/createCharacteristic.ts
@@ -1,13 +1,21 @@
+import { CONFLICT } from '../../../../../lib/constants/http';
+import appAssert from '../../../../../lib/utils/appAssert';
 import {
   SheepCharacteristic,
   SheepCharacteristicModel,
 } from '../../model/sheep.model';
 
 import { CreateCharacteristicSchema } from '../../validation/sheep.schemas';
+import { checkCharacteristicExistsByValue } from '../utils/validations';
 
 export async function createCharacteristic(
   characteristicData: CreateCharacteristicSchema
 ) {
+  const characteristicExists = await checkCharacteristicExistsByValue(
+    characteristicData.value
+  );
+  appAssert(!characteristicExists, CONFLICT, 'Characteristic already exists');
+
   const newCharacteristic: SheepCharacteristic =
     await SheepCharacteristicModel.create(characteristicData);
   return newCharacteristic;
