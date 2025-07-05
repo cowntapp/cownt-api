39b7cdbfe4efd01047106ac4d5906e751415857d
munergenis <munergenis@gmail.com>
2025-07-05 14:41:48 +0200

    feat: add existence check for characteristics in updateCharacteristic function for cow and sheep

--- src/features/animals/cow/service/characteristicMethods/updateCharacteristic.ts
+++ src/features/animals/cow/service/characteristicMethods/updateCharacteristic.ts
@@ -1,14 +1,26 @@
+import { CONFLICT } from '../../../../../lib/constants/http';
+import appAssert from '../../../../../lib/utils/appAssert';
 import {
   CowCharacteristic,
   CowCharacteristicModel,
 } from '../../model/cow.model';
 
 import { UpdateCharacteristicSchema } from '../../validation/cow.schemas';
+import { checkCharacteristicExistsByValue } from '../utils/validations';
 
 export async function updateCharacteristic(
   characteristicId: string,
   characteristicData: UpdateCharacteristicSchema
 ) {
+  const existingCharacteristic = await checkCharacteristicExistsByValue(
+    characteristicData.value
+  );
+  appAssert(
+    !existingCharacteristic,
+    CONFLICT,
+    `Characteristic ${characteristicData.value} already exists`
+  );
+
   const updatedCharacteristic: CowCharacteristic | null =
     await CowCharacteristicModel.findByIdAndUpdate(
       characteristicId,
--- src/features/animals/sheep/service/characteristicMethods/updateCharacteristic.ts
+++ src/features/animals/sheep/service/characteristicMethods/updateCharacteristic.ts
@@ -1,14 +1,26 @@
+import { CONFLICT } from '../../../../../lib/constants/http';
+import appAssert from '../../../../../lib/utils/appAssert';
 import {
   SheepCharacteristic,
   SheepCharacteristicModel,
 } from '../../model/sheep.model';
 
 import { UpdateCharacteristicSchema } from '../../validation/sheep.schemas';
+import { checkCharacteristicExistsByValue } from '../utils/validations';
 
 export async function updateCharacteristic(
   characteristicId: string,
   characteristicData: UpdateCharacteristicSchema
 ) {
+  const existingCharacteristic = await checkCharacteristicExistsByValue(
+    characteristicData.value
+  );
+  appAssert(
+    !existingCharacteristic,
+    CONFLICT,
+    `Characteristic ${characteristicData.value} already exists`
+  );
+
   const updatedCharacteristic: SheepCharacteristic | null =
     await SheepCharacteristicModel.findByIdAndUpdate(
       characteristicId,
