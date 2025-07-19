c8dc484c465114ed0c1e7144921d51d20eded2f8
munergenis <munergenis@gmail.com>
2025-07-19 17:14:50 +0200

    fix: ensure owner field is populated in getCowById function

--- src/features/animals/cow/service/cowMethods/getCowById.ts
+++ src/features/animals/cow/service/cowMethods/getCowById.ts
@@ -4,6 +4,7 @@ export async function getCowById(cowId: string) {
   const cow: Cow | null = await CowModel.findById(cowId).populate([
     'breed',
     'characteristics',
+    'owner',
     'mother',
     'children',
   ]);
