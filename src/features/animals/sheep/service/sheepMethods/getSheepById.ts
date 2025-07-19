ed1b6a5e2a9413dc34d63118d19f8c1f0179fa5f
munergenis <munergenis@gmail.com>
2025-07-19 17:41:41 +0200

    fix: include owner field in sheep population for getSheepById function

--- src/features/animals/sheep/service/sheepMethods/getSheepById.ts
+++ src/features/animals/sheep/service/sheepMethods/getSheepById.ts
@@ -4,6 +4,7 @@ export async function getSheepById(sheepId: string) {
   const sheep: Sheep | null = await SheepModel.findById(sheepId).populate([
     'breed',
     'characteristics',
+    'owner',
     'mother',
     'children',
   ]);
