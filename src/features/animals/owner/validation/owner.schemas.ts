4c388dad4d5dcea2849e039168548fc318f4f216
munergenis <munergenis@gmail.com>
2025-07-19 15:16:10 +0200

    fix: increase maximum length of owner value to 40 characters

--- src/features/animals/owner/validation/owner.schemas.ts
+++ src/features/animals/owner/validation/owner.schemas.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export type CreateOwnerSchema = z.infer<typeof createOwnerSchema>;
 export const createOwnerSchema = z.object({
-  value: z.string().min(2).max(20),
+  value: z.string().min(2).max(40),
 });
 export type UpdateOwnerSchema = z.infer<typeof updateOwnerSchema>;
 export const updateOwnerSchema = createOwnerSchema;
